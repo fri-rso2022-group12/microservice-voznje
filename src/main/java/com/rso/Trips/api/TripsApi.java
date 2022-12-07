8473f7ccead4b775c5ac6cafe824b1ae52418ca1
custibor99 <cus.tibor@outlook.com>
2022-12-07 09:53:25 +0100

    Adding logging

--- src/main/java/com/rso/Trips/api/TripsApi.java
+++ src/main/java/com/rso/Trips/api/TripsApi.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/v1/trips")
@@ -22,45 +23,49 @@ public class TripsApi {
     TripsRepository tripsRepository;
 
     private final Counter apiCallCounter = Metrics.counter("ApiCallCounter");
+    private final Logger logger = Logger.getLogger(TripsApi.class.getName());
 
     @GetMapping("/trip/{id}")
     public TripsModel getTripsById(@PathVariable("id") Long id){
         apiCallCounter.increment();
+        logger.info("getTripsById call");
         TripsModel res = tripsRepository.getByTid(id);
         if(res == null){
             throw new EntityNotFoundException("Entity "+TripsModel.class.toString()+" with id "+id.toString()+" does not exist");
         }
         return res;
-    };
+    }
 
     @GetMapping("/shortest/distance")
     public TripsModel getShortestTripByDistance(){
         apiCallCounter.increment();
+        logger.info("getShortestTripsByDistance call");
         TripsModel res = tripsRepository.getTripsByShortestDistance();
         if(res == null){
             throw new EmptyResultDataAccessException(0);
         }
         return res;
-    };
+    }
 
     @GetMapping("/shortest/time")
     public TripsModel getShortestTripByTime(){
         apiCallCounter.increment();
+        logger.info("getShortestTripByTime call");
         TripsModel res = tripsRepository.getTripsByShortestTime();
         if(res == null){
             throw new EmptyResultDataAccessException(0);
         }
         return res;
-    };
+    }
 
     @GetMapping("/user/{id}")
     public List<TripsModel> getTripsByUser(@PathVariable("id") Long id){
         apiCallCounter.increment();
+        logger.info("getTripsByUser call");
         List<TripsModel> res = tripsRepository.getAllByUid(id);
         if(res == null || res.isEmpty()){
             throw new EntityNotFoundException("Entity "+TripsModel.class.toString()+" with uid "+id.toString()+" does not exist");
         }
         return res;
     }
-
 }
